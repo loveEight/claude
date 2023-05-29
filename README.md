980af08b0ab2f73a34c86fc11d74971aabbd9ae0
loveEight <gg2435684571@gmail.com>
2023-05-29 17:12:59 +0800

    修改注释

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 # chatGPT
-基于 laf 开发的 chatBot 免费 免登录 免翻墙 点击开聊
-
+基于 laf 开发的 claude 免费 免登录 免翻墙 点击开聊
+项目体验地址[claude](https://vcpuq4-claude.site.laf.dev/)
+视频观看地址[5分钟教你搭建Claude网站](https://vcpuq4-claude.site.laf.dev/)
 ## 运行项目
 ```
 npm i
--- src/api/index.js
+++ src/api/index.js
@@ -3,7 +3,7 @@ import axios from "axios";
 // 创建 axios 请求实例
 const serviceAxios = axios.create({
   baseURL: 'https://vcpuq4.laf.dev', // 基础请求地址
-  timeout: 120000, // 请求超时设置
+  timeout: 180000, // 请求超时设置
   withCredentials: false, // 跨域请求是否需要携带 cookie
 });
 
